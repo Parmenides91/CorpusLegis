6aec5959f75e526e269e5e4f788025fd79122e86
Roberto Benéitez Vaquero <robertobv@usal.es>
2026-02-18 20:51:51 +0100

    chore(config): relación de los proyectos

--- CorpusLegis.AppHost/AppHost.cs
+++ CorpusLegis.AppHost/AppHost.cs
@@ -1,7 +1,12 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.CorpusLegis_API>("corpuslegis-api");
 
-builder.AddProject<Projects.CorpusLegis_Web>("corpuslegis-web");
+// Se agrega el proyecto de la API
+var api_corpuslegis = builder.AddProject<Projects.CorpusLegis_API>("corpuslegis-api");
+
+// Se agrega el proyecto del WebApp
+builder.AddProject<Projects.CorpusLegis_Web>("corpuslegis-web")
+        .WithExternalHttpEndpoints()
+        .WithReference(api_corpuslegis);
 
 builder.Build().Run();
