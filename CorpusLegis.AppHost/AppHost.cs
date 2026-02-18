a4dabe23f0ba37cca2ff37d29c98604768d8476c
Roberto Benéitez Vaquero <robertobv@usal.es>
2026-02-18 18:41:18 +0100

    chore: añadir proyecto de API y de web

--- CorpusLegis.AppHost/AppHost.cs
+++ CorpusLegis.AppHost/AppHost.cs
@@ -1,3 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+builder.AddProject<Projects.CorpusLegis_API>("corpuslegis-api");
+
+builder.AddProject<Projects.CorpusLegis_Web>("corpuslegis-web");
+
 builder.Build().Run();
